2b36d3cf39c43ece7ebea3efbe7dd306f08d35aa
melvin <aquinomelvim@gmail.com>
2023-01-06 10:14:23 +0800

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from 'react-icons/bs'
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-4">
+        <div className="conainer-xxl">
+          <div className="row">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-15 align-items-center">
+                <img src="images/newsletter.png" alt="newsletter"/>
+                <h2 className="mb-0 text-white">Sign Up now</h2>
+              </div>
+            </div>
+            <div className="col-7">
+            <div className="input-group mb-3">
+                <input type="text" className="form-control py-1" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                <span className="input-group-text" id="basic-addon2">Subscribe</span>
+            </div>
+            </div>
+
+          </div>
+
+        </div>
+      </footer>
+      <footer className="py-3">
+        <div className="conatiner-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  Metro Manila, Phillipines <br />
+                  Zipcode:1770
+                </address>
+                <a href="tel:+63 9123456789" className="mt-4 d-block mb-0 text-white">+63 9123456789</a><br />
+                <a href="mailto:ammgadget@gmail.com" className="mt-4 d-block mb-3 text-white">ammgadget@gmail.com</a>
+                <div className="social-icons d-flex align-items-center gap-15 mt-4">
+                  <a className="text-white" href="">
+                    <BsLinkedin className="fs-4"/>
+                  </a>
+                  <a className="text-white" href="">
+                    <BsGithub className="fs-4"/>
+                  </a>
+                  <a className="text-white" href="">
+                    <BsYoutube className="fs-4"/>
+                  </a>
+                  <a className="text-white" href="">
+                    <BsInstagram className="fs-4"/>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1" >Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1" >Refund Policy</Link>
+                <Link className="text-white py-2 mb-1" >Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1" >Term and Condition</Link>
+                <Link className="text-white py-2 mb-1" >Blogs</Link>
+              </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1" >About</Link>
+                <Link className="text-white py-2 mb-1" >Shop</Link>
+                <Link className="text-white py-2 mb-1" >Contact</Link>
+              </div>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1" >Laptops</Link>
+                <Link className="text-white py-2 mb-1" >Headphone</Link>
+                <Link className="text-white py-2 mb-1" >Tablets</Link>
+                <Link className="text-white py-2 mb-1" >Smartwatch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="col-12">
+          <p className="text-center mb-0 text-white">
+            &copy: {new Date().getFullYear()};Powered by Ammgadget
+          </p>
+        </div>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
